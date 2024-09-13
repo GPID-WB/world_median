1b85a7de8a0bb07c50fc5e328e5f665523a35ef4
R.Andres Castaneda <acastastanedaa@worldbank.org>
2024-09-13 11:27:51 -0400

    add a few line to estimate poverty in 2024

--- R/world_median.R
+++ R/world_median.R
@@ -12,3 +12,32 @@ dt <- pipapi::pip_grp_logic(povline = wm,
                       country = "WLD",
                       year = 2024,
                       lkup = lkup)
+
+pls <- c(2.15, 30)
+
+cl <- vector("list", length(pls))
+wl <- vector("list", length(pls))
+
+for (i in seq_along(pls)) {
+  pl <- pls[i]
+  cl[[i]] <- pipapi::pip(povline = pl,
+                     year = 2024,
+                     lkup = lkup,
+                     fill_gaps = TRUE)
+
+  wl[[i]] <- pipapi::pip_grp_logic(povline = pl,
+                     country = "WLD",
+                     year = 2024,
+                     lkup = lkup)
+}
+
+cl <- rbindlist(cl, use.names = TRUE)
+wl <- rbindlist(wl, use.names = TRUE)
+
+fs::path(tdirp, "country_2024", ext = "csv") |>
+readr::write_csv(x = cl,
+                 file = _)
+
+fs::path(tdirp, "world_2024", ext = "csv") |>
+readr::write_csv(x = wl,
+                 file = _)
